c46e40c96463826416adf16e9a551b59d1c4089a
Mete Sayan <metesayan99@gmail.com>
2023-08-07 21:24:11 +0300

    feat: lesson module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
-    })
+    }),
+    LessonModule
   ],
 })
 export class AppModule { }
