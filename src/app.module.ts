5200bd7a8711c05fcd84c1938ec096075c99fe3e
Mete Sayan <metesayan99@gmail.com>
2023-08-07 21:22:27 +0300

    feat: GraphQL imported

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    })
+  ],
 })
 export class AppModule { }
